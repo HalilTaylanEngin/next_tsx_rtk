c29a2f2c8cbee0ee62508653f04c0d770aba82a5
HTEngin <tebeskubasing@gmail.com>
2023-10-05 11:33:33 +0300

    added pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div  className='container min-h-screen'>Contact</div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container' >
+    <div className='container min-h-screen' >
       <h1>home page</h1>
     </div>
   )
